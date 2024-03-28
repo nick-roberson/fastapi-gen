ce16af88e7a5700f7dd1b83c8f5c1d293904d6ac
nick-roberson <nicholas.roberson.95@gmail.com>
2024-03-28 12:22:55 -0400

    update readme

--- data/output/nicksapp/src/api/models/index.ts
+++
@@ -1,7 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Group';
-export * from './HTTPValidationError';
-export * from './User';
-export * from './ValidationError';
-export * from './ValidationErrorLocInner';
