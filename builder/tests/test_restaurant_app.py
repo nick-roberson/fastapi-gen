1acf438664df6f555e65516e81ce14acc964af87
Nick Roberson <88335085+nick-roberson@users.noreply.github.com>
2024-05-10 11:21:44 -0400

    Update tests to hit query endpoint (#31)

--- builder/tests/test_restaurant_app.py
+++ builder/tests/test_restaurant_app.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import tempfile
 import time
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import pytest
 import requests
@@ -17,32 +17,85 @@ from builder.managers.linting.manager import LintingManager
 from builder.models.configs import ServiceConfig
 from builder.test_data.create_fake_data import create_fake_data
 
-# Set default values for the service's host, port, and URL
+# Constants for the service's endpoint details
 DEFAULT_HOST = "127.0.0.1"
 DEFAULT_PORT = 8000
 BASE_URL = f"http://{DEFAULT_HOST}:{DEFAULT_PORT}"
 NUM_MODELS = 5  # Number of model instances for testing
 
-# Load configurations from files
+# Load configurations
 TEST_RESTAURANTS_CONFIG_FP = os.path.abspath("builder/tests/configs/restaurant.yaml")
 TEST_RESTAURANT_CONFIG: ServiceConfig = load_and_validate_config(
     TEST_RESTAURANTS_CONFIG_FP
 )
 
-# Prepare parameters for model tests based on configurations
+# Prepare model endpoint parameters
 TEST_RESTAURANT_PARAMS = [
-    (model.name, model.name.lower()) for model in TEST_RESTAURANT_CONFIG.models
+    (model.name, model.name.lower() + "s") for model in TEST_RESTAURANT_CONFIG.models
 ]
 
 
+# Helper functions
+def _get_models(model_name: str) -> List[Dict]:
+    """Get all models from the given endpoint"""
+    url = f"{BASE_URL}/{model_name.lower()}s"
+    print(f"[_get_models] Getting models from {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+    assert response.status_code == 200
+    return response.json()
+
+
+def _get_model(model_name: str, model_id: int) -> Dict:
+    """Get a model instance with the given ID"""
+    url = f"{BASE_URL}/{model_name.lower()}"
+    print(f"[_get_model] Getting model {url}: {model_id}")
+    params = {f"{model_name.lower()}_id": model_id}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    assert response.status_code == 200
+    return response.json()
+
+
+def _create_model(model_name: str, model_data: Dict) -> Dict:
+    """Create a model instance using the given data"""
+    url = f"{BASE_URL}/{model_name.lower()}"
+    print(f"[_create_model] Creating model with data {url}: {model_data}")
+    response = requests.post(url, json=model_data)
+    response.raise_for_status()
+    assert response.status_code == 200
+    return response.json()
+
+
+def _delete_model(model_name: str, model_id: int) -> Dict:
+    """Delete a model instance with the given ID"""
+    url = f"{BASE_URL}/{model_name.lower()}"
+    print(f"[_delete_model] Deleting model {url}: {model_id}")
+    params = {f"{model_name.lower()}_id": model_id}
+    response = requests.delete(url, params=params)
+    response.raise_for_status()
+    assert response.status_code == 200
+    return response.json()
+
+
+def _query_models(model_name: str, query: Dict) -> List[Dict]:
+    """Query models using the given query"""
+    url = f"{BASE_URL}/{model_name.lower()}/query"
+    print(f"[_query_models] Querying models {url}: {query}")
+    response = requests.post(url, json=query)
+    response.raise_for_status()
+    assert response.status_code == 200
+    return response.json()
+
+
+# Fixtures
 @pytest.fixture(scope="module")
 def service():
-    """Setup the application environment, start the service, and perform cleanup."""
     with tempfile.TemporaryDirectory() as output_dir:
         config = copy.copy(TEST_RESTAURANT_CONFIG)
         config.output_dir = output_dir
 
-        # Generate backend code components and setup the environment
+        # Generate and set up the backend service
         generator = BackendGenerator(config=config)
         generator.generate_models()
         generator.generate_services()
@@ -50,22 +103,19 @@ def service():
         generator.generate_database()
         generator.generate_readme()
 
-        # Lint the generated code
         linting_manager = LintingManager(config=config)
         linting_manager.lint_backend()
 
-        # Install project dependencies using Poetry
         poetry_generator = PoetryGenerator(config=config)
         poetry_generator.generate_poetry_toml()
         poetry_generator.install_dependencies()
 
-        # Manage database migrations
         db_manager = DBManager(config=config)
         db_manager.create_migration("Initial migration")
         db_manager.run_migrations()
         db_manager.show_migrations()
 
-        # Start the application server
+        # Start the service
         service_dir = os.path.join(output_dir, "backend")
         proc = subprocess.Popen(
             [
@@ -80,23 +130,18 @@ def service():
             cwd=service_dir,
         )
         time.sleep(5)  # Allow time for the server to start
-
         yield proc, output_dir
-
-        # Terminate the server and cleanup
         proc.kill()
         proc.wait()
 
 
 @pytest.fixture(scope="module")
 def fake_data(service):
-    """Generate and load fake data for use in tests."""
     proc, output_dir = service
     fake_data_paths = create_fake_data(
         config=TEST_RESTAURANT_CONFIG, num=NUM_MODELS, no_ids=True
     )
 
-    # Load the generated fake data from files
     fake_data = {}
     for model_name, fake_data_path in fake_data_paths.items():
         with open(fake_data_path, "r") as f:
@@ -105,70 +150,43 @@ def fake_data(service):
     yield fake_data
 
 
-def test_root_endpoints(service):
-    """Test the health and readiness endpoints of the service."""
-    proc, output_dir = service
-
-    # Test health endpoint
-    response = requests.get(f"{BASE_URL}/health")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Healthy"}
-
-    # Test ready endpoint
-    response = requests.get(f"{BASE_URL}/ready")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Ready"}
-
-
-# Parameterize this test to run for different model types and endpoints
-@pytest.mark.parametrize(
-    "model, endpoint",
-    TEST_RESTAURANT_PARAMS,
-)
+# Tests
+@pytest.mark.parametrize("model_name, endpoint", TEST_RESTAURANT_PARAMS)
 def test_create_and_manage_models(
-    service: Tuple, fake_data: Dict, model: str, endpoint: str
+    service: Tuple, fake_data: Dict, model_name, endpoint: str
 ):
-    """Test the creation, deletion, and listing of model instances."""
-    # Unpack the service tuple
     proc, output_dir = service
 
-    model_data = fake_data[model]
+    # Get the fake data for the model and check it
+    model_data = fake_data[model_name]
     assert model_data
     assert len(model_data) == NUM_MODELS
 
-    # Create one
-    first_instance = model_data[0]
-    response = requests.post(f"{BASE_URL}/{endpoint}", json=first_instance)
-    if response.status_code != 200:
-        print(f"Failed to create {first_instance} with response: {response.text}")
-    assert response.status_code == 200
-    response_json = response.json()
-    assert response_json["id"]
+    # Test model creation and listing
+    created_instances = []
+    for data in model_data:
+        created_instance = _create_model(model_name, data)
+        created_instances.append(created_instance["id"])
 
-    # Create many
-    multiple_instances = model_data[1:5]
-    response = requests.post(f"{BASE_URL}/{endpoint}s", json=multiple_instances)
-    assert response.status_code == 200
-    response_json = response.json()
-    assert len(response_json) == 4
-    assert all("id" in instance and instance["id"] for instance in response_json)
+    # Check that all models were created
+    all_models = _get_models(model_name)
+    assert len(all_models) == NUM_MODELS
 
-    # Query for all instances and check the count
-    response = requests.get(f"{BASE_URL}/{endpoint}s")
-    assert response.status_code == 200
-    response_json = response.json()
-    assert len(response_json) == 5
+    # Check that we can query each model instance
+    for created_id in created_instances:
+        query = {"id": created_id}
+        queried_models = _query_models(model_name, query)
+        assert len(queried_models) == 1
 
-    # Delete one
-    params = {
-        f"{model.lower()}_id": response_json[0]["id"],
-    }
-    print(f"Deleting {model} with params: {params}")
-    response = requests.delete(f"{BASE_URL}/{endpoint}/", params=params)
-    assert response.status_code == 200
+    # Test model deletion
+    for model_id in created_instances:
+        # Delete the model and check if it was removed
+        deleted_model = _delete_model(model_name, model_id)
+        assert deleted_model["id"] == model_id
+        # Check that fetching this model now raises an error
+        with pytest.raises(requests.HTTPError):
+            _get_model(model_name, model_id)
 
-    # Query for all instances and check the count
-    response = requests.get(f"{BASE_URL}/{endpoint}s")
-    assert response.status_code == 200
-    response_json = response.json()
-    assert len(response_json) == 4
+    # Check that getting all models will raise an error
+    with pytest.raises(requests.HTTPError):
+        _get_models(model_name)
